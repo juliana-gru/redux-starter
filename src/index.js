ab8d5597637280b1977d66b5f540e69cc60bef45
Juliana Grutzmann <juliana_grutzmann@hotmail.com>
2021-09-16 12:48:19 -0400

    add new bugResolved action

--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
 import store from './store';
+import { bugAdded, bugRemoved, bugResolved, bugUpdated } from './actions';
 
 const unsubscribe = store.subscribe(() =>{
   console.log('Store changed.', store.getState());
 });
 
-store.dispatch({
-  type: "bugAdded",
-  payload: {
-    description: "Bug1"
-  }
-});
+store.dispatch(bugAdded("Bug 1"));
 
-unsubscribe();
+// unsubscribe();
 
-store.dispatch({
-  type: "bugRemoved",
-  payload: {
-    id: 1
-  }
-})
+store.dispatch(bugResolved(1))
+// store.dispatch(bugRemoved(1))
 
 console.log(store.getState());
--- src/reducer.js
+++ src/reducer.js
@@ -1,7 +1,9 @@
+import * as actions from './actionTypes';
+
 let lastId = 0
 
 export default function reducer(state = [], action) {
-  if (action.type === "bugAdded") {
+  if (action.type === actions.BUG_ADDED) {
     return [
       ...state,
       {
@@ -10,8 +12,15 @@ export default function reducer(state = [], action) {
         resolved: false
       }
     ]
-  } else if (action.type === "bugRemoved") {
+  } 
+  else if (action.type === actions.BUG_REMOVED) {
     return state.filter(bug => bug.id !== action.payload.id)
+  } 
+  else if (action.type === actions.BUG_RESOLVED) {
+    return state.map(bug => bug.id !== action.payload.id 
+      ? bug 
+      : {...bug, resolved: true}
+    );
   }
   return state;
 }
