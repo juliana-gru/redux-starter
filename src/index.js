7c7da14f46b156707f5eabcda628f29d027abf6f
Juliana Grutzmann <juliana_grutzmann@hotmail.com>
2021-09-17 15:32:10 -0400

    ducks pattern

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import store from './customStore';
-import * as actions from './actions';
+import configureStore from './store/configureStore';
+import * as actions from './store/bugs';
+
+const store = configureStore();
 
 store.subscribe(() => {
   console.log('Store changed.')
@@ -7,5 +9,7 @@ store.subscribe(() => {
 
 store.dispatch(actions.bugAdded("Bug 1"));
 store.dispatch(actions.bugAdded("Bug 2"));
+store.dispatch(actions.bugAdded("Bug 3"));
+store.dispatch(actions.bugResolved(1));
 
 console.log(store.getState());
