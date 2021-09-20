09470d3a0c7d416fb6591d672b0d41c50f247269
Juliana Grutzmann <juliana_grutzmann@hotmail.com>
2021-09-20 10:12:28 -0400

    refactor to use createSlice from redux toolkit

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,54 +1,33 @@
-//DUCKS PATTERN
+import { createSlice } from "@reduxjs/toolkit";
+//with createSlice you don't have to explicitly create actions on a reducer.
 
-//Action types
-const BUG_ADDED = "bugAdded";
-const BUG_REMOVED = "bugRemoved";
-const BUG_RESOLVED = "bugResolved";
-
-//Action creators 
-export const bugAdded = description => ({
-  type: BUG_ADDED,
-  payload: {
-    description
-  }
-});
-
-export const bugRemoved = id => ({
-  type: BUG_REMOVED,
-  payload: {
-    id
-  }
-});
-
-export const bugResolved = id => ({
-  type: BUG_RESOLVED,
-  payload: {
-    id
-  }
-})
-
-//Reducer
 let lastId = 0
 
-export default function reducer(state = [], action) {
-  if (action.type === BUG_ADDED) {
-    return [
-      ...state,
-      {
+const slice = createSlice({
+  name: "bugs",
+  initialState: [],
+  reducers: {
+    // maps actions => action handlers
+    bugAdded: (bugs, action) => {
+      //you can write mutating code here cuz the redux toolkit will transform it into immutable
+      bugs.push({
         id: ++lastId,
         description: action.payload.description,
         resolved: false
-      }
-    ]
-  } 
-  else if (action.type === BUG_REMOVED) {
-    return state.filter(bug => bug.id !== action.payload.id)
-  } 
-  else if (action.type === BUG_RESOLVED) {
-    return state.map(bug => bug.id !== action.payload.id 
-      ? bug 
-      : {...bug, resolved: true}
-    );
+      });
+    },
+
+    bugResolved: (bugs, action) => {
+      const index = bugs.findIndex(bug => bug.id === action.payload.id);
+      bugs[index].resolved = true;
+    },
+
+    bugRemoved: (bugs, action) => {
+      return bugs.filter(bug => bug.id !== action.payload.id)
+    }    
   }
-  return state;
-}
+})
+
+export const {bugAdded, bugResolved, bugRemoved} = slice.actions;
+export default slice.reducer;
+
