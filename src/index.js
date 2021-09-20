5efe8971c93dbd720cf931891055160466a6bf71
Juliana Grutzmann <juliana_grutzmann@hotmail.com>
2021-09-20 14:55:12 -0400

    add users slice

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 import configureStore from './store/configureStore';
-import * as actions from './store/bugs';
+import { 
+  bugAdded, 
+  bugResolved, 
+  bugRemoved, 
+  bugAssignedToUser, 
+  getBugsByUser, 
+  getUnresolvedBugs
+} from './store/bugs';
+import{ projectAdded }from './store/projects';
+import { userAdded } from './store/users';
 
-console.log(actions)
 const store = configureStore();
 
 store.subscribe(() => {
   console.log('Store changed.')
 })
- 
-store.dispatch(actions.bugAdded({ description: "Bug 1" }));
-store.dispatch(actions.bugAdded({ description:"Bug 2" }));
-store.dispatch(actions.bugAdded({ description:"Bug 3" }));
-store.dispatch(actions.bugResolved({ id: 1 }));
 
-console.log(store.getState());
+store.dispatch(userAdded({ name: "Petra" }));
+store.dispatch(userAdded({ name: "Mel" }));
+
+
+store.dispatch(projectAdded({ name: "Project 1" })); 
+
+store.dispatch(bugAdded({ description:"Bug 1" }));
+store.dispatch(bugAdded({ description:"Bug 2" }));
+store.dispatch(bugAdded({ description:"Bug 3" }));
+store.dispatch(bugAssignedToUser({ userId: 1, bugId: 1 }));
+store.dispatch(bugResolved({ id: 1 }));
+
+const unresolvedBugs = getUnresolvedBugs(store.getState());
+const bugs = getBugsByUser(1)(store.getState());
+console.log(unresolvedBugs);
+console.log(bugs);
--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,5 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 //with createSlice you don't have to explicitly create actions on a reducer.
+import { createSelector } from 'reselect';
+
 
 let lastId = 0
 
@@ -17,6 +19,12 @@ const slice = createSlice({
       });
     },
 
+    bugAssignedToUser: (bugs, action) => {
+      const { bugId, userId } = action.payload;
+      const index = bugs.findIndex(bug => bug.id === bugId);
+      bugs[index].userId = userId;
+    },
+
     bugResolved: (bugs, action) => {
       const index = bugs.findIndex(bug => bug.id === action.payload.id);
       bugs[index].resolved = true;
@@ -26,8 +34,20 @@ const slice = createSlice({
       return bugs.filter(bug => bug.id !== action.payload.id)
     }    
   }
-})
+});
 
-export const {bugAdded, bugResolved, bugRemoved} = slice.actions;
+export const { bugAdded, bugResolved, bugRemoved, bugAssignedToUser } = slice.actions;
 export default slice.reducer;
 
+//Selector
+
+  //Memoization
+export const getUnresolvedBugs = createSelector(
+  state => state.entities.bugs, 
+  bugs => bugs.filter(bug => !bug.resolved)
+);
+
+export const getBugsByUser = userId => createSelector(
+  state => state.entities.bugs,
+  bugs => bugs.filter(bug => bug.userId === userId)
+);
