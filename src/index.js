227e4b66a9267dff1232278bc905c893b919eeca
Juliana Grutzmann <juliana_grutzmann@hotmail.com>
2021-09-22 15:08:41 -0400

    refactor store

--- src/index.js
+++ src/index.js
@@ -1,48 +1,6 @@
 import configureStore from './store/configureStore';
-import * as actions from './store/api';
-import { 
-  bugAdded, 
-  bugResolved, 
-  bugRemoved, 
-  bugAssignedToUser, 
-  getBugsByUser, 
-  getUnresolvedBugs
-} from './store/bugs';
-import{ projectAdded }from './store/projects';
-import { userAdded } from './store/users';
+import { loadBugs } from './store/bugs';
 
 const store = configureStore();
 
-store.dispatch(
-  actions.apiCallBegan({
-  url: '/bugs',
-  method: 'get',
-  onSuccess: 'bugsReceived',
-}));
-
-// store.dispatch({
-//   type: "error",
-//   payload: { message: "An error ocurred." }
-// });
-
-
-// store.subscribe(() => {
-//   console.log('Store changed.')
-// })
-
-// store.dispatch(userAdded({ name: "Petra" }));
-// store.dispatch(userAdded({ name: "Mel" }));
-
-
-// store.dispatch(projectAdded({ name: "Project 1" })); 
-
-// store.dispatch(bugAdded({ description:"Bug 1" }));
-// store.dispatch(bugAdded({ description:"Bug 2" }));
-// store.dispatch(bugAdded({ description:"Bug 3" }));
-// store.dispatch(bugAssignedToUser({ userId: 1, bugId: 1 }));
-// store.dispatch(bugResolved({ id: 1 }));
-
-// const unresolvedBugs = getUnresolvedBugs(store.getState());
-// const bugs = getBugsByUser(1)(store.getState());
-// console.log(unresolvedBugs);
-// console.log(bugs);
+store.dispatch(loadBugs());
--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,18 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 //with createSlice you don't have to explicitly create actions on a reducer.
 import { createSelector } from 'reselect';
-
+import { apiCallBegan } from './api';
 
 let lastId = 0
 
 const slice = createSlice({
   name: "bugs",
-  initialState: [],
+  initialState: {
+    list: [],
+    loading: false,
+    lastFetch: null
+  },
   reducers: {
     // maps actions => action handlers
+    bugsReceived: (bugs, action) => {
+      bugs.list = action.payload;
+    },
     bugAdded: (bugs, action) => {
       //you can write mutating code here cuz the redux toolkit will transform it into immutable
-      bugs.push({
+      bugs.list.push({
         id: ++lastId,
         description: action.payload.description,
         resolved: false
@@ -21,33 +28,42 @@ const slice = createSlice({
 
     bugAssignedToUser: (bugs, action) => {
       const { bugId, userId } = action.payload;
-      const index = bugs.findIndex(bug => bug.id === bugId);
-      bugs[index].userId = userId;
+      const index = bugs.list.findIndex(bug => bug.id === bugId);
+      bugs.list[index].userId = userId;
     },
 
     bugResolved: (bugs, action) => {
-      const index = bugs.findIndex(bug => bug.id === action.payload.id);
-      bugs[index].resolved = true;
+      const index = bugs.list.findIndex(bug => bug.id === action.payload.id);
+      bugs.list[index].resolved = true;
     },
 
     bugRemoved: (bugs, action) => {
-      return bugs.filter(bug => bug.id !== action.payload.id)
+      return bugs.list.filter(bug => bug.id !== action.payload.id)
     }    
   }
 });
 
-export const { bugAdded, bugResolved, bugRemoved, bugAssignedToUser } = slice.actions;
+export const { bugAdded, bugResolved, bugRemoved, bugAssignedToUser, bugsReceived } = slice.actions;
 export default slice.reducer;
+
+//Action creators
+const url = "/bugs";
+export const loadBugs = () => apiCallBegan({
+  url,
+  onSuccess: bugsReceived.type,
+});
+
+
 
 //Selector
 
   //Memoization
 export const getUnresolvedBugs = createSelector(
   state => state.entities.bugs, 
-  bugs => bugs.filter(bug => !bug.resolved)
+  bugs => bugs.list.filter(bug => !bug.resolved)
 );
 
 export const getBugsByUser = userId => createSelector(
   state => state.entities.bugs,
-  bugs => bugs.filter(bug => bug.userId === userId)
+  bugs => bugs.list.filter(bug => bug.userId === userId)
 );
